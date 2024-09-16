ac5be6135008186f56713631420110f751695b07
Gregory Jenkins <gregoryjenk@gmail.com>
2024-09-16 19:43:37 +1000

    Adding build model object and test.

---
+++ src/GregoryJenk.Workflow.Model.Test/Builds/BuildTest.cs
@@ -0,0 +1,28 @@
+﻿using GregoryJenk.Workflow.Model.Builds;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace GregoryJenk.Workflow.Model.Test.Builds
+{
+    [TestClass]
+    public class BuildTest
+    {
+        [TestMethod]
+        public void IsVersion()
+        {
+            //Arrange.
+            var build = new Build()
+            {
+                Created = DateTimeOffset.Now,
+                Version = 1
+            };
+
+            //Act.
+            var actualResult = build.IsVersion(1);
+
+            //Assert.
+            Assert.AreEqual(true, actualResult);
+        }
+    }
+}
---
+++ src/GregoryJenk.Workflow.Model/Builds/Build.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace GregoryJenk.Workflow.Model.Builds
+{
+    public class Build
+    {
+        public DateTimeOffset Created { get; set; }
+
+        public int Version { get; set; }
+
+        public bool IsVersion(int version)
+        {
+            try
+            {
+                return Version == version;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
